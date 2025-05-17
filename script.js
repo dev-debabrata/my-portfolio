b0b26277d07124d178cfba22739357c700c7274e
dev-debabrata <debabratadas711@gmail.com>
2025-05-18 01:42:30 +0530

    18-05-2025

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+
+window.onload = function () {
+    window.scrollTo(0, 0);
+};
+
+
 function toggleMobileMenu() {
     document.getElementById("menu").classList.toggle("active");
 }

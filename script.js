6b1d1844e92ef421686d6f12953da2d37e5b9d53
dev-debabrata <debabratadas711@gmail.com>
2025-05-26 23:16:46 +0530

    18-05-2025

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-if ('scrollRestoration' in history) {
-    history.scrollRestoration = 'manual';
+// if ('scrollRestoration' in history) {
+//     history.scrollRestoration = 'manual';
+// }
+
+// window.onload = function () {
+//     window.scrollTo(0, 0);
+// };
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
 }
 
-window.onload = function () {
-    window.scrollTo(0, 0);
-};
+// function toggleMobileMenu() {
+//     document.getElementById("menu").classList.toggle("active");
+// }
 
+// function name(params) {
 
-function toggleMobileMenu() {
-    document.getElementById("menu").classList.toggle("active");
-}
-
-function name(params) {
-
-}
+// }
